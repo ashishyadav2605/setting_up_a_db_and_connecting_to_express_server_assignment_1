042772dd15cbc2683c96f9370310e76cb19af70a
Ashish <iashishyadav17@gmai.com>
2025-02-19 16:31:40 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const Port = process.env.PORT || 9000;
+const DB_url = process.env.DB_URL;
 
 app.use(express.static('static'));
+
+mongoose.connect(DB_url
+
+
+  
+)
+  .then(() => console.log("Connected to database"))
+  .catch(err => console.error("Error connecting to database:", err));
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(Port, () => {
+  console.log(`Example app listening at http://localhost:${Port}`);
 });
